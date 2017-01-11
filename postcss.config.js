96b8092912ea39f5548a150e427d6639817aed4d
Alexander Baran <alexanderbaran@live.com>
2017-01-11 17:52:42 +0100

    asdf

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({
-            browsers: ['last 10 version', '> 2%']
+            browsers: ['last 10 version']
         })
     ]
 }
