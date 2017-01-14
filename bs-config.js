9689e294cb8dd284bab7ec6cfdeef980e2226b90
Alexander Baran <alexanderbaran@live.com>
2017-01-14 18:24:07 +0100

    asdf

---
+++ bs-config.js
@@ -0,0 +1,14 @@
+// https://browsersync.io/docs/options/
+module.exports = {
+    port: 8080,
+
+    server: {
+        baseDir: './dist'
+    },
+
+    // Stop the browser from automatically opening
+    open: false,
+
+    // Disable UI completely
+    ui: false
+};
