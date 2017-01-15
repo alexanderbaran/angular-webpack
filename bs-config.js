de3f259dfa33ab7ca7731f669c5ed1b3bb7ab5f0
Alexander Baran <alexanderbaran@live.com>
2017-01-15 16:49:52 +0100

    Added proxy to bs-config.js

--- bs-config.js
+++ bs-config.js
@@ -1,9 +1,20 @@
+// https://www.npmjs.com/package/http-proxy-middleware
+var proxy = require('http-proxy-middleware');
+
+var apiProxy = proxy('/api/', {
+    target: 'http://localhost:8080',
+    secure: false,
+    changeOrigin: true, // needed for virtual hosted sites
+    ws: true // proxy websockets
+});
+
 // https://browsersync.io/docs/options/
 module.exports = {
     port: 80,
 
     server: {
-        baseDir: './dist'
+        baseDir: './dist',
+        middleware: [apiProxy]
     },
 
     // Stop the browser from automatically opening
