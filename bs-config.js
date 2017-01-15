a649b001af4ed6d042668393e3d74dfd6af2a3f1
Alexander Baran <alexanderbaran@live.com>
2017-01-15 16:54:51 +0100

    asdf

--- bs-config.js
+++ bs-config.js
@@ -1,20 +1,20 @@
-// https://www.npmjs.com/package/http-proxy-middleware
-var proxy = require('http-proxy-middleware');
+// // https://www.npmjs.com/package/http-proxy-middleware
+// var proxy = require('http-proxy-middleware');
 
-var apiProxy = proxy('/api/', {
-    target: 'http://localhost:8080',
-    secure: false,
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true // proxy websockets
-});
+// var apiProxy = proxy('/api/', {
+//     target: 'http://localhost:8080',
+//     secure: false,
+//     changeOrigin: true, // needed for virtual hosted sites
+//     ws: true // proxy websockets
+// });
 
 // https://browsersync.io/docs/options/
 module.exports = {
     port: 80,
 
     server: {
-        baseDir: './dist',
-        middleware: [apiProxy]
+        baseDir: './dist'
+        // middleware: [apiProxy]
     },
 
     // Stop the browser from automatically opening
